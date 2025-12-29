5fd33c7538f6f11c689a05535b7aa28b3c8773a1
kunalkian <kunalkkr192004@gmail.com>
2025-12-29 17:28:36 +0530

    Cooking JavaScript

---
+++ JavaScript/15_Control_Flow/if.js
@@ -0,0 +1,60 @@
+// // if
+// const isUserloggedIN=true
+// const temperature=41
+
+// // if(2!=3){
+// //     console.log("executed");;
+    
+// // }
+
+// if(temperature === 41)
+// {
+//     console.log("less than 50");
+// }
+
+// // console.log("Temperature is greater than 50");
+// console.log("Execute");
+
+// // <, >, <=, >=, ==, !=, ===, !==
+
+// const score=200
+
+// if(score>100)
+// {
+//     const power ="fly"
+//     console.log(`My power is to ${power}`);    
+// }
+
+// yeh thik hai par isko khi mat likhna good practice nhi hai
+// const balance=1000
+// if(balance >500) console.log("test"),console.log("test2");
+
+// const balance=1000
+// if(balance <500)
+// {
+//     console.log("less than 500");
+// }
+// else if(balance <750)
+// {
+//     console.log("less than 750");
+// }
+// else if(balance<900)
+// {
+//     console.log("less than 900");
+// }
+// else
+// {
+//     console.log("less than 1000");
+// }
+
+const userLoggedIn =true
+const debitCard=true
+const loggedInFromGoogle=false
+const loggedInFromEmail=true
+if(userLoggedIn && debitCard && 2==2)
+{
+    console.log("Allow to buy course");    
+}
+if(userLoggedIn || loggedInFromEmail){
+    console.log("Allow to Log In");
+}
